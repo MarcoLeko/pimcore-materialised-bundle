86782b007c3cd0c54b672910b9ca0c201ea83f4a
lekoma <marco.leko@jsmd-group.com>
2025-02-17 07:32:26 +0100

    create repo

---
+++ src/PimcoreMaterialisedBundle.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ */
+
+namespace Pimcore\Bundle\MaterialisedBundle;
+
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
+
+class PimcoreMaterialisedBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
+{
+    use BundleAdminClassicTrait;
+    use PackageVersionTrait;
+
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    public function getJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getCssPaths(): array
+    {
+        return [
+            '/bundles/pimcorematerial/css/material.css',
+        ];
+    }
+
+    public function getEditmodeCssPaths(): array
+    {
+        return $this->getCssPaths();
+    }
+
+    public function getEditmodeJsPaths(): array
+    {
+        return $this->getJsPaths();
+    }
+}
